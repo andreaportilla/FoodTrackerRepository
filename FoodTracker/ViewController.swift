c4bad9db1ffcefa14ba8e052967bfaf9483b9b7a
Andréa Portilla <andreaportilla@macdrea.local>
2018-10-30 23:15:12 -0400

    commit5

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
 
@@ -48,7 +48,27 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextFieldDelegate
         
         // Make sure ViewController is notified when the user picks an image.
         imagePickerController.delegate = self
-        present(imagePickerController, animated: true, completion: nil)g;;;;;
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            // The info dictionary may contain multiple representations of the image. You want to use the original.
+            guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+            }
+            
+            // Set photoImageView to display the selected image.
+            photoImageView.image = selectedImage
+            
+            // Dismiss the picker.
+            dismiss(animated: true, completion: nil)
+        }
+        
     }
     
     //Mark: Actions
