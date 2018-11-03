2a1f7d60df4d3224a3a7361c7a9cc08f4ed10dc6
Andréa Portilla <andreaportilla@macdrea.local>
2018-11-03 16:38:54 -0400

    commit7

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,10 +13,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     //MARK: Properties
 
     @IBOutlet weak var nameTextfield: UITextField!
-    
     @IBOutlet weak var mealNameLabel: UILabel!
     
-    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
    
