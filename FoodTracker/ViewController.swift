71086dc8a9182e6010454adb9c350b266cc9b978
Andréa Portilla <andreaportilla@macdrea.local>
2018-11-02 22:46:15 -0400

    commit6

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     @IBAction func selectImageromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         // Hide the keyboard.
-        nameTextField.resignFirstResponder()
+        nameTextfield.resignFirstResponder()
         
         // UIImagePickerController is a view controller that lets a user pick media from their photo library.
         let imagePickerController = UIImagePickerController()
@@ -53,12 +53,14 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     //MARK: UIImagePickerControllerDelegate
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        // Dismiss the picker if the user canceled.
-        dismiss(animated: true, completion: nil)
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            // Dismiss the picker if the user canceled.
+            dismiss(animated: true, completion: nil)
+            func imagePickerController(_ picker: UIImagePickerController,
+                                       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
             // The info dictionary may contain multiple representations of the image. You want to use the original.
-            guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            guard let selectedImage = info[.originalImage] as? UIImage else {
                 fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
             }
             
