8b24dc795bf5df3cdfc3ea522322d9f17700490b
Andréa <andreaportillao@gmail.com>
2018-11-13 18:28:48 -0500

    'To add an action to the button' ERROR
    Does not run and wants to fix the problem with something that contains 'Objective-C ' ??

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,6 +10,10 @@ class RatingControl: UIStackView {
         super.init(coder: coder)
         setupButtons()
     }
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
     //MARK: Private Methods
     private func setupButtons() {
         // Create the button
@@ -19,6 +23,8 @@ class RatingControl: UIStackView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         // Add the button to the stack
         addArrangedSubview(button)
     }
