76185ed3a80d20942f20ba1be17fcde126f67df0
Andréa <andreaportillao@gmail.com>
2018-11-13 18:55:05 -0500

    Stopped at step 3 ERROR
    Maybe error, not sure

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -6,6 +6,8 @@ import UIKit
     private var ratingButtons = [UIButton]()
     
     var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+    @IBInspectable var starCount: Int = 5
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,14 +23,14 @@ import UIKit
     }
     //MARK: Private Methods
     private func setupButtons() {
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
         // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
         // Add constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+        button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
         // Setup the button action
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         // Add the button to the stack
