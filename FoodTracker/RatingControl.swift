525845892dc8eb128fbf095a7f93e1a9e3a0941c
Andréa <andreaportillao@gmail.com>
2018-11-13 18:02:27 -0500

    'To override the initializers'
    Title of the step in the tutorial. Runs.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,11 +1,10 @@
 import UIKit
 
 class RatingControl: UIStackView {
-    
+    //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
     required init(coder: NSCoder) {
         super.init(coder: coder)
     }
